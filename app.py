f850ba06f5ca5c774af752f0357cac81880cc109
jhermon <suddyjerm@gmail.com>
2020-01-31 17:13:51 -0500

    created my first Flask app

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ app = Flask(__name__)
 def home():
     return 'My home page'
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
@@ -24,4 +28,4 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0",port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
